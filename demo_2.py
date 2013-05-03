ddca7ffa5d9288f99a23341bdf64939e77ea318b
mmgp <mmgpnih@gmail.com>
2013-05-03 15:37:32 -0300

    Clean up (attempt to prepare demo_2 to be reusable).

--- demo_2.py
+++ demo_2.py
@@ -1,38 +1,41 @@
 from __future__ import print_function
 
-import sys
 import sqlite3
-from twisted.internet import reactor
 
 # Own modules
-from btcx import mtgox, cfgmanager
+from btcx import mtgox
 
 
 class TradeFetchStore(object):
-    def __init__(self, db, table, mtgox, last_tid=0):
+    def __init__(self, db, table, mtgox, verbose=1):
+        self.verbose = verbose
+
         self.db = db
         self.table = table
         self.cursor = db.cursor()
         self.mtgox = mtgox
-        self.mtgox.evt.listen('connected', self.on_connect)
+        self.mtgox.evt.listen('connected', self.load_from_last_stored)
         self.mtgox.evt.listen('trade_fetch', self.on_trade_fetch)
-        self.cursor.execute("""SELECT tid FROM %s
+        self.cursor.execute("""SELECT tid FROM [%s]
                 ORDER BY tid DESC LIMIT 1""" % self.table)
         res = self.cursor.fetchone()
         self.prev_last = None
         self.last_tid = 0 if res is None else res[0]
-        print(self.last_tid)
+        if verbose:
+            print(self.last_tid)
 
-    def on_connect(self, _):
+    def load_from_last_stored(self, ignore=True):
         self.mtgox.load_trades_since(tid=self.last_tid)
 
     def on_trade_fetch(self, (tid, timestamp, ttype, price, amount, coin)):
         if tid is None:
             self.db.commit()
-            print("tid", self.last_tid, self.prev_last, "<<")
+            if self.verbose:
+                print("tid", self.last_tid, self.prev_last, "<<")
             if self.prev_last == self.last_tid:
-                print("Got it all, congrats.")
-                reactor.stop()
+                if self.verbose:
+                    print("Got it all, congrats.")
+                self.mtgox.evt.emit('done', None)
             else:
                 # Ask for more!
                 self.prev_last = self.last_tid
@@ -44,32 +47,34 @@ class TradeFetchStore(object):
             # Trade in a different currency or not primary.
             return
 
-        #print("trade", tid, timestamp, ttype, price, amount)
-        self.cursor.execute("INSERT INTO %s VALUES (?, ?, ?, ?, ?)"%self.table,
+        if self.verbose > 5:
+            print("trade", tid, timestamp, ttype, price, amount)
+        self.cursor.execute("INSERT INTO [%s] VALUES (?,?,?,?,?)" % self.table,
                 (tid, timestamp, ttype, str(price), str(amount)))
 
 
-def main():
-    currency = 'USD' # Changing this to 'EUR' should work just fine.
-
-    table = "btc%s" % (currency.lower())
-    db = sqlite3.connect('mtgox_trades.db')
-    cursor = db.cursor()
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS %s (
+def setup_database(db, tablename):
+    db.execute("""CREATE TABLE IF NOT EXISTS [%s] (
             tid INTEGER PRIMARY KEY, timestamp TIMESTAMP,
-            ttype TEXT, price TEXT, amount TEXT)""" % table)
-    cursor.execute("""
-            CREATE INDEX IF NOT EXISTS %s_ts ON %s (timestamp)
-            """ % (table, table))
+            ttype TEXT, price TEXT, amount TEXT)""" % tablename)
+    db.execute("""
+            CREATE INDEX IF NOT EXISTS [%s_ts] ON [%s] (timestamp)
+            """ % (tablename, tablename))
     db.commit()
+
+def setup_client(currency='USD', dbname='mtgox_trades.db'):
+    table = "btc%s" % currency.lower()
+    db = sqlite3.connect(dbname)
+    setup_database(db, table)
 
     mtgox_client = mtgox.create_client('', '', currency) # No key/secret.
     tradefetch = TradeFetchStore(db, table, mtgox_client)
-    mtgox.start(mtgox_client)
-
-    reactor.run()
+    return mtgox_client, tradefetch
 
 
 if __name__ == "__main__":
-    main()
+    from twisted.internet import reactor
+    mtgox_client, tradefetch = setup_client()
+    mtgox_client.evt.listen('done', lambda _: reactor.stop())
+    mtgox.start(mtgox_client)
+    reactor.run()
