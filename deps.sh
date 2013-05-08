fd09e4358d6290f331f15c4ac6c6237607614828
mmgp <mmgpnih@gmail.com>
2013-05-07 23:06:46 -0300

    Corrected information about packages used.

--- deps.sh
+++ deps.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Dependencies:
-#  * scrypt: used for storing API key secret
+#  * scrypt: used for storing API key secret or any other sensitive data
 #  * autobahn: Websocket for Twisted, used for MtGox
-#  * treq: simple way for making HTTP requests through Twisted, used for BTC-e
+#  * treq: simple way for making HTTP requests through Twisted
 
 DEST_DIR="dep"
 EASY_INSTALL="easy_install-2.7"
