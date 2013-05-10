57f5ae265a92ebf3ac80f58b0229d276dd659129
mmgp <mmgpnih@gmail.com>
2013-05-10 18:04:37 -0300

    Refactored to a function.

--- misc/sqlite_to_hdf5.py
+++ misc/sqlite_to_hdf5.py
@@ -1,42 +1,47 @@
 import os
-import sys
 import pandas
 import sqlite3
 from pandas.io import sql as psql
 
+def sqlite_table_to_hdf5(db_name, table):
+    h5_name = os.path.splitext(db_name)[0]
+    h5_group = '%s_%s' % (h5_name, table)
 
-DB = sys.argv[1]
-TABLE = 'btcusd'
+    h5_out = '%s.h5' % h5_name
+    print "Storage: %s   Group: %s" % (h5_out, h5_group)
+    store = pandas.HDFStore(h5_out)
 
-h5_name = os.path.splitext(DB)[0]
-h5_group = '%s_%s' % (h5_name, TABLE)
+    storer = store.get_storer('/%s' % h5_group)
+    if storer is not None:
+        # Already stored something, grab last transaction tid.
+        last_item = store.select(h5_group, start=storer.nrows - 1)
+        last_tid = int(last_item.index[0][1])
+    else:
+        last_tid = -1 # all-time data. This might take some time.
+    print "Last transaction ID: %d" % last_tid
 
-store = pandas.HDFStore('%s.h5' % h5_name)
-
-storer = store.get_storer('/%s' % h5_group)
-if storer is not None:
-    # Already stored something, grab last transaction tid.
-    last_item = store.select(h5_group, start=storer.nrows - 1)
-    last_tid = int(last_item.index[0][1])
-else:
-    last_tid = -1 # all-time data. This might take some time.
-
-db = sqlite3.connect(DB, detect_types=sqlite3.PARSE_COLNAMES)
-db.text_factory = str
-query = """SELECT datetime(timestamp, 'unixepoch') as "ts [timestamp]",
+    db = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_COLNAMES)
+    query = """SELECT datetime(timestamp, 'unixepoch') as "ts [timestamp]",
         CAST(price as FLOAT) as price, CAST(amount as FLOAT) as volume, tid
-        FROM [%s] WHERE tid > ? ORDER BY tid ASC LIMIT 5000""" % TABLE
+        FROM [%s] WHERE tid > ? ORDER BY tid ASC LIMIT 5000""" % table
 
-while True:
-    dataframe = psql.frame_query(query, con=db, params=(last_tid, ),
-            index_col=['ts', 'tid'])
+    while True:
+        dataframe = psql.frame_query(query, con=db, params=(last_tid, ),
+                index_col=['ts', 'tid'])
 
-    if not len(dataframe) or last_tid == dataframe.index[-1][1]:
-        print "Done"
-        break
+        if not len(dataframe) or last_tid == dataframe.index[-1][1]:
+            print "Done."
+            break
 
-    print last_tid, dataframe.index[-1][1], len(dataframe)
-    store.append(h5_group, dataframe)
-    last_tid = int(dataframe.index[-1][1])
+        print last_tid, dataframe.index[-1][1], len(dataframe)
+        store.append(h5_group, dataframe)
+        last_tid = int(dataframe.index[-1][1])
 
-store.close()
+    store.close()
+
+
+if __name__ == "__main__":
+    import sys
+    dbname = sys.argv[1]
+    table = sys.argv[2] if len(sys.argv) > 2 else 'btcusd'
+    sqlite_table_to_hdf5(dbname, table)
